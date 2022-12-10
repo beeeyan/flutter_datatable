bc14c3324c55089f542dd7a37ad5a2aeb5b9eb26
sakasanokuma <moonsunphoenix@gmail.com>
2022-12-10 23:17:12 +0900

    datatable_2の表示

--- lib/sample/datatable_2_sample.dart
+++ lib/sample/datatable_2_sample.dart
@@ -1,7 +1,9 @@
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datatable/controller/article_controller.dart';
 import 'package:flutter_datatable/widget/drawer_menu.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 
 class DataTable2Sample extends ConsumerWidget {
   const DataTable2Sample({super.key});
@@ -30,10 +32,63 @@ class DataTable2Sample extends ConsumerWidget {
               left: 20,
               right: 20,
             ),
-            child: Center(
-              child: Text(
-                articleList.toString(),
-              ),
+            child: DataTable2(
+              // DataTable Classと使い方に違いはない。
+              columns: const [
+                DataColumn(
+                  label: Text('id'),
+                ),
+                DataColumn(
+                  label: Text('タイトル'),
+                ),
+                DataColumn(
+                  label: Text('作成日'),
+                ),
+                DataColumn(
+                  label: Text('更新日'),
+                ),
+              ],
+
+              rows: articleList
+                  .map(
+                    (article) => DataRow(
+                      cells: <DataCell>[
+                        DataCell(
+                          Text(article.id),
+                        ),
+                        DataCell(
+                          Text(article.title),
+                        ),
+                        DataCell(
+                          Text(
+                            DateFormat.yMMMMd()
+                                .add_jms()
+                                .format(article.createdAt),
+                          ),
+                        ),
+                        DataCell(
+                          Text(
+                            DateFormat.yMMMMd()
+                                .add_jms()
+                                .format(article.updatedAt),
+                          ),
+                        ),
+                      ],
+                      // メモ「value」がbool値なのが、人によっては扱いづらいかも。
+                      onSelectChanged: (value) {
+                        showDialog<void>(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('選択した行のデータ'),
+                              content: Text(article.toString()),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  )
+                  .toList(),
             ),
           );
         },
